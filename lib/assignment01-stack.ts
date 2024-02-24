f0d350986de39f11c85dfe0f49089ddea780b2bc
Jessica McCabe <mccabejessica88@gmail.com>
2024-02-24 13:56:14 +0000

    Initial Dynamo DB setup

--- lib/assignment01-stack.ts
+++ lib/assignment01-stack.ts
@@ -1,6 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import { Construct } from 'constructs';
+import * as custom from "aws-cdk-lib/custom-resources";
+import { generateBatch } from "../shared/util";
+import {movieReview} from "../seed/movieReview";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class Assignment01Stack extends cdk.Stack {
@@ -9,13 +12,32 @@ export class Assignment01Stack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    const movieReviews = new dynamodb.Table(this, "movieReviews", {
+    const movieReviewsTable = new dynamodb.Table(this, "movieReviews", {
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
       partitionKey: { name: "MovieId", type: dynamodb.AttributeType.NUMBER },
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       tableName: "movieReviews",
     });
+
+    new custom.AwsCustomResource(this, "movieReviewsddbInitData", {
+      onCreate: {
+        service: "DynamoDB",
+        action: "batchWriteItem",
+        parameters: {
+          RequestItems: {
+            [movieReviewsTable.tableName]: generateBatch(movieReview),
+          },
+        },
+        physicalResourceId: custom.PhysicalResourceId.of("movieReviewsddbInitData"), //.of(Date.now().toString()),
+      },
+      policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
+        resources: [movieReviewsTable.tableArn],
+      }),
+    });
+    
   }
+
+  
 
   
 }
---
+++ seed/movieReview.ts
@@ -0,0 +1,11 @@
+import {MovieReview} from '../shared/types'
+
+export const movieReview : MovieReview[] = [
+  {
+    MovieId: 1,
+    ReviewerName: 'Bob Hunt',
+    ReviewDate: '2/24/2024',
+    Content: 'Great Movie',
+    Rating: 5
+  }
+]
