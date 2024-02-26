8e509edb7522919e2a23df50c73d81e2442d1021
Jessica McCabe <mccabejessica88@gmail.com>
2024-02-26 19:21:00 +0000

    Update movieReview type - further breakdown for review date and review body

--- seed/movieReview.ts
+++ seed/movieReview.ts
@@ -5,18 +5,21 @@ export const movieReview : MovieReview[] = [
     movieId: 1,
      review:  [
         {
-      reviewerName: 'Bob Hunt',
-      reviewDate: '2/24/2024',
-      content: 'Great Movie',
-      rating: 5
-   
+        reviewDate: '2/24/2024',
+        reviewBody:{
+        reviewerName: 'Bob Hunt',
+        content: 'Great Movie',
+        rating: 5
+        }
   },
   {
-    reviewerName: 'Rachel Worm',
     reviewDate: '2/26/2024',
+    reviewBody:{
+    reviewerName: 'Rachel Worm',
     content: 'Spectacular',
     rating: 5
   }
+}
 
   
 ]
--- shared/types.d.ts
+++ shared/types.d.ts
@@ -7,8 +7,12 @@ export type MovieReview =   {
 }
 
 type Review = {
-  reviewerName: string,
   reviewDate: string,
+  reviewBody: ReviewBody
+}
+
+  type ReviewBody = {  
+  reviewerName: string,
   content: string,
   rating: 1 | 2 | 3| 4| 5
 }
